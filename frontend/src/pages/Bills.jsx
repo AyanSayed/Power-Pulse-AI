ffea72dbca00c846e3e559cc83d8d92e1bc2887f
Ayan Sayed <ayansayed37@gmail.com>
2026-08-01 11:55:06 +0530

    Fix missing QuickLinkCard import in Bills.jsx

--- frontend/src/pages/Bills.jsx
+++ frontend/src/pages/Bills.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useBill } from "../context/BillContext";
+import QuickLinkCard from "../components/QuickLinkCard";
 import {
   FaCalendarAlt,
   FaBolt,
